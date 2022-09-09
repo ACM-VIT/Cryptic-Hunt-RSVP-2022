c7d22cdbd8857b6a1a2726c341098c3d0a63628d
Harshal <ranjhaniharshal@gmail.com>
2022-09-09 22:13:59 +0530

    add tailwind setup

---
+++ rsvp/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-4xl p-6 text-orange-600 underline">
+    Hello world!
+    </h1>
+  );
+}
+
+export default App;
