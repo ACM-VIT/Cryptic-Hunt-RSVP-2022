e324b832163a435ff891a227969521fe0313df3f
tejas273 <bentenison1098@gmail.com>
2022-09-19 14:40:36 +0530

    feat: add footer and faq

--- rsvp/src/App.js
+++ rsvp/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Footer from './components/Footer/Footer';
+import FaqSection from './components/Faq/faqSection';
 
 function App() {
   return (
-    <h1 className="text-4xl p-6 text-orange-600 underline">
-    Hello world!
-    </h1>
+    <div>
+      <FaqSection/>
+      <Footer/>
+    </div>
   );
 }
 
